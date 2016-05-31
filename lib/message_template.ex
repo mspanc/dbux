414f9e603f53d442aead33435e822f9eda2d8c9e
Marcin Lewandowski <marcin@radiokit.org>
2016-05-31 12:02:08 +0000

    Fixed arguments order in MessageTemplate

--- lib/message_template.ex
+++ lib/message_template.ex
@@ -9,7 +9,7 @@ defmodule DBux.MessageTemplate do
   Builds standard method call to org.freedesktop.DBus.Hello.
   """
   @spec hello(DBux.Serial.t) :: %DBux.Message{}
-  def hello(serial) when is_number(serial) do
+  def hello(serial \\ 0) when is_number(serial) do
     DBux.Message.build_method_call(serial, "/org/freedesktop/DBus", "org.freedesktop.DBus", "Hello", [], "org.freedesktop.DBus")
   end
 
@@ -17,8 +17,8 @@ defmodule DBux.MessageTemplate do
   @doc """
   Builds standard method call to org.freedesktop.DBus.RequestName.
   """
-  @spec request_name(DBux.Serial.t, String.t, number) :: %DBux.Message{}
-  def request_name(serial, name, flags \\ 0) when is_number(serial) and is_binary(name) and is_number(flags) do
+  @spec request_name(String.t, number, DBux.Serial.t) :: %DBux.Message{}
+  def request_name(name, flags \\ 0, serial \\ 0) when is_number(serial) and is_binary(name) and is_number(flags) do
     DBux.Message.build_method_call(serial, "/org/freedesktop/DBus", "org.freedesktop.DBus", "RequestName", [
       %DBux.Value{type: :string, value: name},
       %DBux.Value{type: :uint32, value: flags}
