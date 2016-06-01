164849ba59b6d2e58a0f1cecd0095efb1868a09c
Marcin Lewandowski <marcin@radiokit.org>
2016-06-01 15:42:11 +0200

    Bumped to 1.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DBux.Mixfile do
 
   def project do
     [app: :dbux,
-     version: "1.0.2",
+     version: "1.0.3",
      elixir: "~> 1.0",
      description: description,
      package: package,
